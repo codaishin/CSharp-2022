a17f1eceb22f7df0672288ae89865f8bac772cba
codaishin <codaishin@gmail.com>
2022-05-16 18:59:30 +0200

    feat: add fibonacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿var first = 1;
+var second = 1;
+var third = 0;
+var pattern = "{0} ";
+
+Console.Write(pattern, first);
+Console.Write(pattern, second);
+
+for (int i = 2; i < 20; ++i) {
+	third = first + second;
+	Console.Write(pattern, third);
+	first = second;
+	second = third;
+}
---
+++ fibonacci/Reamde.md
@@ -0,0 +1,4 @@
+# Task
+
+- display the first 20 fibonacci number
+- no self written functions allowed yet

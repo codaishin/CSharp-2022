6f7ff9682b62d89eca4f7faf09fbd8f10190265e
Mike Arnhold <mike.arnhold@tu-dresden.de>
2022-05-16 13:44:29 +0200

    feat(Quiz): add

---
+++ quiz/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welche Farbe hat grünes Grassssss?");
+Console.WriteLine("a: blua, b: grün, c: gelb, d: pink");
+
+var repeat = true;
+
+do
+{
+	var input = Console.ReadLine();
+	switch (input)
+	{
+		case "a":
+		case "c":
+		case "d":
+			Console.WriteLine("Falsch, u fool");
+			repeat = false;
+			break;
+		case "b":
+			Console.WriteLine("Richtig, u rock");
+			repeat = false;
+			break;
+		default:
+			Console.WriteLine("U SUCK, U CAN DO NOTHING RIGHT!!!!");
+			break;
+	}
+} while (repeat);

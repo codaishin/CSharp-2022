6405c647eb9f75fd04184c896d5811c637a39a4d
codaishin <codaishin@gmail.com>
2022-05-17 19:44:54 +0200

    feat(json): add

---
+++ json/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+record Question {
+	public string Text { get; init; } = "";
+	public string[] Answers { get; init; } = new string[0];
+	public int CorrectAnswer { get; init; } = -1;
+}
+
+class Program {
+	public static void Main() {
+		var text = File.ReadAllText("data.json");
+		var data = JsonSerializer.Deserialize<Question>(text)!;
+		Console.WriteLine(data.Text);
+		Console.WriteLine(string.Join(", ", data.Answers));
+		Console.WriteLine(data.CorrectAnswer);
+	}
+}

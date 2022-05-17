fd58fbc9c43f0990e93302fa39887385db3fa769
Mike Arnhold <mike.arnhold@tu-dresden.de>
2022-05-17 15:20:25 +0200

    refactor(quiz): make it amazing

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -1,24 +1,67 @@
-﻿Console.WriteLine("What color is green grass?");
-Console.WriteLine("a: blue, b: green, c: yellow, d: pink");
+﻿class Program {
 
-var repeat = true;
-
-do {
-	var input = Console.ReadLine();
-	switch (input) {
-		case "a":
-		case "c":
-		case "d":
-			Console.WriteLine("Wrong. (really? you got that wrong?)");
-			Console.WriteLine("The correct answer is: green");
-			repeat = false;
-			break;
-		case "b":
-			Console.WriteLine("Correct, u rock");
-			repeat = false;
-			break;
-		default:
-			Console.WriteLine("Choose a valid answer... (what a fool)");
-			break;
+	struct Question {
+		public string text;
+		public string answerA;
+		public string answerB;
+		public string answerC;
+		public string correctAnswer;
 	}
-} while (repeat);
+
+	enum Status { CORRECT, INCORRECT, ERROR }
+
+	static string ToString(Question question) {
+		return $"{question.text}\na: {question.answerA}, b: {question.answerB}, c: {question.answerC}";
+	}
+
+	static string ToString(Question question, Status status) {
+		switch (status) {
+			case Status.CORRECT:
+				return "Correct!";
+			case Status.INCORRECT:
+				return $"Wong! The correct answer is {question.correctAnswer}";
+			default:
+				return "Invalid Answer, Try again";
+		}
+	}
+
+	static Status Evaluate(Question question, string input) {
+		input = input.ToLower();
+		if (input == question.correctAnswer.ToLower()) {
+			return Status.CORRECT;
+		}
+		if (input == "a" || input == "b" || input == "c") {
+			return Status.INCORRECT;
+		}
+		return Status.ERROR;
+	}
+
+	static bool ShouldRepeat(Status status) {
+		return status == Status.ERROR;
+	}
+
+
+	static void Foo(string value) {
+
+	}
+
+	public static void Main() {
+
+		Status status;
+		var question = new Question {
+			text = "What color is green grass?",
+			answerA = "blue",
+			answerB = "green",
+			answerC = "pink",
+			correctAnswer = "c",
+		};
+
+		Console.WriteLine(Program.ToString(question));
+
+		do {
+			var input = Console.ReadLine()!;
+			status = Program.Evaluate(question, input);
+			Console.WriteLine(Program.ToString(question, status));
+		} while (Program.ShouldRepeat(status));
+	}
+}

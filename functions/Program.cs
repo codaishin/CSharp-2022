08ad262ec7080f0ec73c53c43fcdfc201bb10f74
Mike Arnhold <mike.arnhold@tu-dresden.de>
2022-05-20 08:57:54 +0200

    feat(function): addPerson

--- functions/Program.cs
+++ functions/Program.cs
@@ -1,18 +1,45 @@
-﻿static int Power(int @base, int exponent) {
-	var power = 1;
-	if (exponent < 0) {
-		throw new NegativeExponent("exponent must be positive");
-	}
-	for (int i = 0; i < exponent; ++i) {
-		power *= @base;
-	}
-	return power;
+﻿// static int Power(int @base, int exponent) {
+// 	var power = 1;
+// 	if (exponent < 0) {
+// 		throw new NegativeExponent("exponent must be positive");
+// 	}
+// 	for (int i = 0; i < exponent; ++i) {
+// 		power *= @base;
+// 	}
+// 	return power;
+// }
+
+// var result = Power(4, -1);
+// Console.WriteLine($"4 ^ 5 = {result}");
+
+
+// class NegativeExponent : Exception {
+// 	public NegativeExponent(string msg) : base(msg) { }
+// }
+
+
+var people = new List<Person>();
+
+AddPerson(people, 42, "Hugo");
+AddPerson(people, 33, "Henriette");
+
+foreach (var person in people) {
+	Console.WriteLine(person.name);
+	Console.WriteLine(person.age);
 }
 
-var result = Power(4, -1);
-Console.WriteLine($"4 ^ 5 = {result}");
+static void AddPerson(List<Person> personList, int age, string name) {
+	var person = new Person {
+		name = name,
+		age = age,
+	};
+	// var person = new Person();
+	// person.age = age;
+	// person.name = name;
+	personList.Add(person);
+}
 
-
-class NegativeExponent : Exception {
-	public NegativeExponent(string msg) : base(msg) { }
+struct Person {
+	public string name;
+	public int age;
 }

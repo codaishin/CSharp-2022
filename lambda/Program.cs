1c6bbcda788768cf268a94b555b6041b200b4928
Mike Arnhold <mike.arnhold@tu-dresden.de>
2022-05-20 11:53:57 +0200

    feat(lambda): add

---
+++ lambda/Program.cs
@@ -0,0 +1,10 @@
+﻿var value = "aikiokiiikalii";
+var count = value.Count(c => c == 'i');
+Console.WriteLine($"count: {count}");
+
+var numbers = new[] { 1, 2, 3, 4 };
+var filtered = numbers.Where(n => n % 2 == 0);
+
+foreach (var number in filtered) {
+	Console.WriteLine(number);
+}
